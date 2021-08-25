93f853a166466580f58e7fea2aee98b05b76f4da
Utkarsh Bopche <utkarsh.bopche@thinkitive.com>
2021-08-25 19:16:34 +0530

    Initial Commit

---
+++ SettingsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingsApp
+//
+//  Created by Apple on 25/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
